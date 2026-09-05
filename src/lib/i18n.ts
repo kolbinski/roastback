0aa10187007ff8e9590ff75937237aea1c9c95f2
Krzysztof Olbinski <olbik80@gmail.com>
2026-09-05 16:20:24 +0200

    Fixes

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -57,7 +57,7 @@ export const translations: Record<
     cancelLabel: "Cancel",
   },
   pl: {
-    heroCopy: "Pokaż mi, co masz.<br />Powiem Ci, kim dziś jesteś.",
+    heroCopy: "Pokaż mi, co masz.<br />Powiem Ci, kim dzisiaj jesteś.",
     buttonLabel: "Roast Back",
     userBubble: "Hej RoastBack.app, zroastuj moje zdjęcie, bez litości!",
     loadingText: "Roastuję... to może chwilę potrwać, zamierzam być bezlitosny.",
